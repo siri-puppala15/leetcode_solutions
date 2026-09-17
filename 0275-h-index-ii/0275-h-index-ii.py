93931d01606b319183662fde3bed8824f12d1099
Puppala Sireesha <sirisuresh.puppala@gmail.com>
2026-09-17 20:17:44 +0530

    Time: 0 ms (100%), Space: 15 MB (17.65%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hIndex(self, citations):
+        n = len(citations)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if citations[mid] >= n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return n - left
